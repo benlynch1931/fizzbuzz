c68927142d501b85dfd3602793edc4149f9ca9d8
Ben <ben.lynch1931@gmail.com>
2020-09-22 12:23:06 +0100

    Code passes first 5 basic tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,22 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "fizz " when passed number divisible by 3' do
+    expect(fizzbuzz(348)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed number divisible by 5' do
+    expect(fizzbuzz(1465)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+
 end
